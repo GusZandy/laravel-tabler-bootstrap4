90d03b76c1660ad1f1370cd0d1d9167921a3ee04
GusZandy <zandy.yudha.p@gmail.com>
2019-08-22 10:09:39 +0700

    Implemented panel component for example.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,20 @@
 @extends('layouts.app')
 
+@section('page-title', 'Home')
+
 @section('content')
-<div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
+            @component('components.panel', [
+                'title' => 'Welcome',
+            ])
+                @slot('tools')
+                    <a href="#" class="card-options-collapse" data-toggle="card-collapse"><i
+                            class="fe fe-chevron-up"></i></a>
+                    <a href="#" class="card-options-remove" data-toggle="card-remove"><i class="fe fe-x"></i></a>
+                @endslot
+                You are logged in!
+            @endcomponent
         </div>
     </div>
-</div>
 @endsection
--- resources/views/layouts/content-wrap/content.blade.php
+++ resources/views/layouts/content-wrap/content.blade.php
@@ -2,8 +2,11 @@
     <div class="container">
         <div class="page-header">
             <h1 class="page-title">
-                Dashboard
+                @yield('page-title')
             </h1>
+        </div>
+        <div class="page-content">
+            @yield('content')
         </div>
     </div>
 </div>
